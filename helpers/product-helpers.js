f0d18d099c6a87ec8fb8123431cb22dbcf219323
hafizmp <hafiznjh007@gmail.com>
2022-09-30 22:16:08 +0530

    Added Edit image function in Admin page

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -30,5 +30,30 @@ module.exports = {
                 
             });
         })
+    },
+
+    getProductDetails: (proId) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)})
+            .then((product) => {
+                resolve(product)
+            });
+        })
+    },
+
+    updateProduct: (proId, proDetails) => {
+        return new Promise((resolve, reject) => {
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name: proDetails.Name,
+                    Category: proDetails.Category,
+                    Price: proDetails.Price,
+                    Description: proDetails.Description,                    
+                }
+            }).then((response)=>{
+                resolve(response)
+            })
+        })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -36,7 +36,7 @@ router.get('/delete-product/:id', (req, res) => {
   productHelpers.deleteProduct(productId).then((response) => {
     res.redirect('/admin')
   })
-  
+
   const path = './public/images/product-images/' + productId + '.jpg'
 
   try {
@@ -45,6 +45,24 @@ router.get('/delete-product/:id', (req, res) => {
   } catch (err) {
     console.error(err)
   }
+})
+
+router.get('/edit-product/:id', async (req, res) => {
+  let product = await productHelpers.getProductDetails(req.params.id)
+  console.log(product);
+  res.render('admin/edit-product', { product })
+})
+
+router.post('/edit-product/:id', (req, res) => {
+  let id = req.params.id
+  productHelpers.updateProduct(req.params.id, req.body)
+    .then(() => {
+      res.redirect('/admin')
+      if (req.files.Image) {
+        let image = req.files.Image
+        image.mv('./public/images/product-images/' + id + '.jpg')
+      }
+    })
 })
 
 module.exports = router;
