6ddd63bf1163081cbe43999bd99b5637211a8041
hafizmp <hafiznjh007@gmail.com>
2022-10-02 12:56:59 +0530

    Added count in cart navLink

--- routes/user.js
+++ routes/user.js
@@ -13,12 +13,17 @@ const verifyLogin = (req, res, next) => {
 }
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
   let user = req.session.user
   console.log(user);
+  let cartCount = null
+  if (req.session.user) {
+    cartCount = await userHelper.getCartCount(req.session.user._id)
+  }
+  
   productHelper.getAllProducts()
     .then((products) => {
-      res.render('user/view-products', { title: 'Shopping Cart', products, user, admin: false })
+      res.render('user/view-products', { title: 'Shopping Cart', products, user, cartCount, admin: false })
     })
 });
 
@@ -70,17 +75,17 @@ router.get('/logout', (req, res) => {
 })
 
 
-router.get('/cart', verifyLogin,async (req, res) => {
+router.get('/cart', verifyLogin, async (req, res) => {
   let products = await userHelper.getCartProducts(req.session.user._id)
   console.log(products);
-  res.render('user/cart')
+  res.render('user/cart', { title: 'Your cart', products, user: req.session.user })
 })
 
 router.get('/add-to-cart/:id', verifyLogin, (req, res) => {
   userHelper.addToCart(req.params.id, req.session.user._id)
-  .then(() => {
-    res.redirect('/')
-  });
+    .then(() => {
+      res.redirect('/')
+    });
 })
 
 
